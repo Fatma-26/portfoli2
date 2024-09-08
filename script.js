a71e85188342861c0911261462b954fd9d9692a0
Fatma-26 <yasmeenyahya26@gmail.com>
2024-09-09 02:24:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var x= document.getElementById("idbody");
+var y= document.getElementById("icon");
+
+console.log(y);
+function dark(){
+   if(x.style.backgroundColor==="white")
+   { 
+    x.style.backgroundColor="black"}
+   else
+    x.style.backgroundColor="white"
+}
